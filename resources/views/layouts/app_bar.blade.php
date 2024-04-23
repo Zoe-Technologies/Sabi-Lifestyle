2022777e1205ea65fc718d70b4e3fd012773a21e
Dearie <eburujacintha@gmail.com>
2024-04-23 22:30:03 +0100

    feat: completed layout feature

--- resources/views/layouts/app_bar.blade.php
+++ resources/views/layouts/app_bar.blade.php
@@ -299,9 +299,7 @@
                             </div>
                         </div>
                         <!-- end page title -->
-
-                     
-
+                        @yield('content')
 
                         
                     </div>
@@ -317,7 +315,7 @@
                             </div>
                             <div class="col-sm-6">
                                 <div class="text-sm-end d-none d-sm-block">
-                                    Design & Developed by <a href="" class="text-decoration-underline">ZoeTechnologies</a>
+                                  Developed by <a href="" class="text-decoration-underline">ZoeTechnologies</a>
                                 </div>
                             </div>
                         </div>
